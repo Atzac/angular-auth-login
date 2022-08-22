591afce866008f562cf778bc37f7166f6955e956
Atzac <isaacquidutemelo13@gmail.com>
2022-08-22 11:11:12 -0300

    logOut

--- auth-login-angular/src/app/core/services/auth.service.ts
+++ auth-login-angular/src/app/core/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -8,16 +9,27 @@ import { catchError, map, Observable, throwError } from 'rxjs';
 export class AuthService {
   private url: string = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   public sign(payload: { email: string; password: string }): Observable<any> {
-    return this.http.post(`${this.url}/sign`, payload).pipe(
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payload).pipe(
       map((data) => {
-        return console.log(data);
+        localStorage.removeItem("access_token")
+        localStorage.setItem("access_token", data.token)
+        return this.router.navigate(['admin'])
       }),
       catchError(err => {
-        return throwError(() => err.error.message);
+        if (err.error.message) return throwError(() => err.error.message);
+        return throwError(() => "Erro interno!");
       })
     )
+  }
+
+  public logOut() {
+    localStorage.removeItem("access_token")
+    return this.router.navigate([''])
   }
 }
